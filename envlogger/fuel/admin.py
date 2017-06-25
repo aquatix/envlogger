715d3a68d76f3b786eb09cec5e69c7a98efafe10
Michiel Scholten <michiel@diginaut.net>
2017-06-25 20:30:02 +0200

    Car and defaults

--- envlogger/fuel/admin.py
+++ envlogger/fuel/admin.py
@@ -2,14 +2,22 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import FuelStation, Refuelling
+from .models import Car, FuelStation, Refuelling, Default
 
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('label', )
 
 class FuelStationAdmin(admin.ModelAdmin):
     list_display = ('label', )
 
 class RefuellingAdmin(admin.ModelAdmin):
-    list_display = ('date', 'litres', 'km_per_litre', 'price', 'station', )
+    list_display = ('date', 'litres', 'km_per_litre', 'price', 'station', 'car', )
 
+class DefaultsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'car', 'station', )
+
+admin.site.register(Car, CarAdmin)
 admin.site.register(FuelStation, FuelStationAdmin)
 admin.site.register(Refuelling, RefuellingAdmin)
+admin.site.register(Default, DefaultsAdmin)
--- envlogger/fuel/models.py
+++ envlogger/fuel/models.py
@@ -12,6 +12,22 @@ class BaseModel(models.Model):
         abstract = True
 
 
+class Car(BaseModel):
+    """Car"""
+
+    user = models.ForeignKey(User)
+    label = models.CharField(max_length=200)
+    number_plate = models.CharField(max_length=20)
+    make = models.CharField(max_length=30, null=True, blank=True)
+    model = models.CharField(max_length=30, null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+    notes = models.CharField(max_length=512, null=True, blank=True)
+
+
+    def __unicode__(self):
+        return self.label
+
+
 class FuelStation(BaseModel):
     """Fuel station"""
 
@@ -27,6 +43,7 @@ class FuelStation(BaseModel):
 class Refuelling(BaseModel):
     """Refuelling event"""
 
+    car = models.ForeignKey(Car)
     station = models.ForeignKey(FuelStation)
     date = models.DateTimeField()
     litres = models.DecimalField(max_digits=9, decimal_places=3)
@@ -39,13 +56,29 @@ class Refuelling(BaseModel):
     notes = models.CharField(max_length=512, null=True, blank=True)
 
     @property
-    def km_per_litre(self):
+    def km_per_litre_float(self):
         return self.daycounterkm / self.litres
+
+    @property
+    def km_per_litre(self):
+        return '{0:.2f}'.format(self.km_per_litre_float)
 
     @property
     def litre_per_100km(self):
         return self.litres / (self.daycounterkm / 100.0)
 
     @property
+    def show_litre_per_100km(self):
+        return '{0:.2f}'.format(self.litre_per_100km)
+
+    @property
     def price_per_km(self):
         return self.price / self.daycounterkm
+
+
+class Default(BaseModel):
+    """Default Car and FuelStation for User"""
+
+    user = models.OneToOneField(User)
+    car = models.ForeignKey(Car)
+    station = models.ForeignKey(FuelStation)
